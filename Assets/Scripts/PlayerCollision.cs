a8ca6ca6d7d4e440c924e4081f545edaa4c2ebbd
Bebbo <mrbebz@gmail.com>
2019-09-01 08:10:17 +0300

    Update 3.1
    Ice shader
    Render Pipeline

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -4,16 +4,64 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
+    public List<GameObject> meshes = new List<GameObject>();
     public PlayerMovement movement;
+    public int hits = 3;
+    float cooldown = 1f;
+    float hitTime;
+    GameManager gameManager;
+    GameObject activeMesh;
+
+    private void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+        hitTime = Time.time;
+
+        if (hits > meshes.Count)
+            Debug.Log("mesh count doesn't match the hit count.");
+        else
+            DecideChildMesh();
+    }
+
     void OnCollisionEnter(Collision collisionInfo)
     {
         if (collisionInfo.collider.tag == "Obstacle")
         {
-           // Debug.Log("we hit an obstacle");
-            movement.enabled = false;
+            if (Time.time - hitTime < cooldown)
+                return;
 
-            FindObjectOfType<GameManager>().EndGame();
+            hitTime = Time.time;
+            --hits;
+
+            if (hits <= 0)
+            {
+                hits = 0;
+                EndGame();
+            }
+            else
+            {
+                Destroy(collisionInfo.gameObject);
+                DecideChildMesh();
+            }
         }
+    }
 
+    void DecideChildMesh()
+    {
+        if ((hits <= meshes.Count) && (hits >= 0))
+        {
+            if (activeMesh != null)
+                Destroy(activeMesh);
+
+            Debug.Log("hits: " + hits);
+
+            activeMesh = GameObject.Instantiate(meshes[hits - 1], transform.position, transform.rotation, transform);
+        }
+    }
+
+    void EndGame()
+    {
+        movement.enabled = false;
+        gameManager?.EndGame();
     }
 }
