62f764186ecac181dfa4f84ee1e43ff4020f0219
Bebbo <mrbebz@gmail.com>
2019-10-03 08:22:59 +0300

    3.9.4
    - fix movement when crash with obstacle
    - trying to move child mesh up and down

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,25 +4,33 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Rigidbody rb;
+    public Rigidbody player;
+    public Transform playerMelting;
+    public MeshRenderer meltingMesh;
     public float forwardForce = 2000f;
     public float sidewaysForce = 500f;
     void FixedUpdate()
     {
-        rb.AddForce(0,0, forwardForce * Time.deltaTime);
+        player.AddForce(0,0, forwardForce * Time.deltaTime);
         if (Input.GetKey("d"))
         {
-            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            player.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
         if (Input.GetKey("a"))
         {
-            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            player.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
 
         //Melting
-        //if (Time.timeSinceLevelLoad)
-        //{
-
-        //}
+        float timeToMelt = 0f;
+        if (Time.timeSinceLevelLoad > timeToMelt)
+        {
+            //make render move down
+            playerMelting.position.Set(playerMelting.position.x, playerMelting.position.y - 0.03f, playerMelting.position.z); // not working
+            //meltingMesh.
+            Debug.Log(player.position);
+            timeToMelt = Time.timeSinceLevelLoad;    
+            
+        }
     }
 }
