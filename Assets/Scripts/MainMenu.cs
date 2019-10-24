1dd42450564226da0ec44ef6da89a3d84fe30bec
Bebbo <mrbebz@gmail.com>
2019-10-25 00:29:57 +0300

    Patch 4.0.2.2
    main menu finished

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,19 +4,14 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void PlayGame()
     {
-        
+        Debug.Log("scene is loading");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Quit()
     {
-        
-    }
-    public void ToGame()
-    {
-        SceneManager.LoadScene("Game");
+        Application.Quit();
     }
 }
